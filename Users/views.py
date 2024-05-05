3a6c3af30715b809b6bc614ac3fb7dfe8d0ec1e5
manhhd <manhhd.uet@gmail.com>
2024-05-06 03:55:58 +0700

    finish create ui

--- Users/views.py
+++ Users/views.py
@@ -5,6 +5,7 @@ from .forms import LoginForm, SignUpForm
 from django.views import View
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.models import User
 
 class Login_View(View):
     def get(self, request):
@@ -13,6 +14,7 @@ class Login_View(View):
         return render(request, "accounts/login.html", {"form": form, "msg": msg})
     def post(self, request):
         form = LoginForm(request.POST or None)
+        msg = None
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
@@ -22,7 +24,6 @@ class Login_View(View):
                 return redirect("../../home")
             else:
                 msg = "Error: Username or Password is incorrect"
-        msg = "Login failed!"
         return render(request, "accounts/login.html", {"form": form, "msg": msg})
 class Register_User(View):
     def get(self, request):
@@ -30,5 +31,30 @@ class Register_User(View):
         msg = None
         success = False
         return render(request, "accounts/register.html", {"form": form, "msg": msg})
+    def post(self, request):
+        msg=None
+        success=False
+        form = SignUpForm(request.POST)
+        print(form)
+        if form.is_valid():
+            print("valid")
+        else:
+            print("not valid")
+        if form.is_valid():
+            #form.save()
+            print("valid")
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            #user = authenticate(username=username, password=raw_password)
+            print("username + password + user: ")
+            print(username, + " " + raw_password)
+            print(user)
+            if user is not None:
+                msg = 'User created.'
+                success = True
+        else:
+            msg = 'Form is not valid'
+        return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
+
 
     
